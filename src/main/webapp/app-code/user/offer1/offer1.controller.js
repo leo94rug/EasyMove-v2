9e2a5c70668866801b700e0cde09b8e44d258877
Leonardo <master_94@virgilio.it>
2017-12-31 17:09:57 +0100

    bug fix date in offer1;

--- src/main/webapp/app-code/user/offer1/offer1.controller.js
+++ src/main/webapp/app-code/user/offer1/offer1.controller.js
@@ -2,87 +2,98 @@
     'use strict';
 
     angular
-            .module('app')
-            .controller('Offer1Controller', Offer1Controller);
-    Offer1Controller.$inject = ['$timeout','$q','$scope', 'UserService', '$location', '$rootScope', '$mdpDatePicker', '$mdpTimePicker', 'AuthenticationService','$store','FlashService'];
-    function Offer1Controller($timeout,$q,$scope, UserService, $location, $rootScope, $mdpDatePicker, $mdpTimePicker, AuthenticationService,$store,FlashService) {
+        .module('app')
+        .controller('Offer1Controller', Offer1Controller);
+    Offer1Controller.$inject = ['$timeout', '$q', 'UserService', '$location', '$rootScope', '$mdpDatePicker', '$mdpTimePicker', 'AuthenticationService', '$store', 'FlashService'];
+    function Offer1Controller($timeout, $q, UserService, $location, $rootScope, $mdpDatePicker, $mdpTimePicker, AuthenticationService, $store, FlashService) {
         var vm = this;
-        vm.color="red";
-        vm.utente = $store.get('utente');
-        $('body,html').animate({scrollTop:0},800);
-        $timeout(function(){
-            var myEl = angular.element( document.querySelector( '#headerOffer' ) );
-            myEl.addClass('active');           
-        });      
-        vm.radioData = [
-            {label: 'Andata', value: 1},
-            {label: 'Andata e ritorno', value: 2},
-        ];        
-        vm.partenzaList=[{0:vm.partenza}];        
-        vm.arrivoList=[{0:vm.arrivo}];
-        vm.tappeList = [];        
-        vm.tappeModel = [
-            {1:vm.tappa1},
-            {2:vm.tappa2},
-            {3:vm.tappa3},
-            {4:vm.tappa4}
-        ];
-        vm.data = {
-            group2: 1,
-            type:''
-        };
-        vm.contTappa=0;
-        vm.submit = submit;
-        vm.localizzato = 0;
         vm.geolocate = geolocate;
         vm.addTappa = addTappa;
+        vm.submit = submit;
         vm.removeTappa = removeTappa;
-        vm.types=''
         vm.logout = logout;
-        vm.initialize=initialize;
-        vm.andataTratta = [];
-        vm.ritornoTratta = [];
+        vm.initialize = initialize;
         vm.initialize();
-        function initialize(){
-            if ($rootScope.saveOffer!=undefined) {
-                vm.partenzaList=$rootScope.saveOffer.partenza;
-                vm.tappeList=$rootScope.saveOffer.tappe;
-                vm.arrivoList=$rootScope.saveOffer.arrivo;
-                vm.andataTime=$rootScope.saveOffer.dateTimeA;
-                vm.ritornoTime=$rootScope.saveOffer.dateTimeR;
-                vm.data.group2=$rootScope.saveOffer.ar;
+        function initialize() {
+            vm.dataprecerror=false;
+            vm.today = new Date();
+            vm.color = "red";
+            vm.utente = $store.get('utente');
+            $('body,html').animate({ scrollTop: 0 }, 800);
+            $timeout(function () {
+                var myEl = angular.element(document.querySelector('#headerOffer'));
+                myEl.addClass('active');
+            });
+            vm.radioData = [
+                { label: 'Andata', value: 1 },
+                { label: 'Andata e ritorno', value: 2 },
+            ];
+            vm.partenzaList = [{ 0: vm.partenza }];
+            vm.arrivoList = [{ 0: vm.arrivo }];
+            vm.tappeList = [];
+            vm.tappeModel = [
+                { 1: vm.tappa1 },
+                { 2: vm.tappa2 },
+                { 3: vm.tappa3 },
+                { 4: vm.tappa4 }
+            ];
+            vm.data = {
+                group2: 1,
+                type: ''
+            };
+            vm.contTappa = 0;
+            vm.localizzato = 0;
+            vm.types = ''
+            vm.andataTratta = [];
+            vm.ritornoTratta = [];
+            if ($rootScope.saveOffer != undefined) {
+                vm.partenzaList = $rootScope.saveOffer.partenza;
+                vm.tappeList = $rootScope.saveOffer.tappe;
+                vm.arrivoList = $rootScope.saveOffer.arrivo;
+                vm.andataTime = $rootScope.saveOffer.dateTimeA;
+                vm.ritornoTime = $rootScope.saveOffer.dateTimeR;
+                vm.data.group2 = $rootScope.saveOffer.ar;
             }
         }
         function addTappa() {
-            if (vm.contTappa<4) {
-                vm.tappeList.push({ model: vm.tappeModel[vm.contTappa]})
+            if (vm.contTappa < 4) {
+                vm.tappeList.push({ model: vm.tappeModel[vm.contTappa] })
                 vm.contTappa++;
             }
         }
         function removeTappa(n) {
-            vm.tappeList.splice(n, 1); 
+            vm.tappeList.splice(n, 1);
             vm.contTappa--;
         }
         function logout() {
             AuthenticationService.ClearCredentials();
-            FlashService.set({title: "Logout effettuato", body: "", type: "info"});
+            FlashService.set({ title: "Logout effettuato", body: "", type: "info" });
             $location.path('/');
         }
         function submit() {
-            if ($scope.offer.$valid) {
+            debugger;
+            if (vm.data.group2 === 2) {
+
+                if (vm.andataTime > vm.ritornoTime) {
+                    vm.offer.$valid = false;
+                    vm.dataprecerror=true;
+                }
+            }
+            if (vm.offer.$valid) {
+                vm.dataprecerror=false;                
                 var promises = [];
-                var enumeratore=vm.partenzaList.concat(vm.tappeList, vm.arrivoList);
-                for (var i = 0; i <enumeratore.length; i++) {
-                    enumeratore[i].numero=i;
+                var enumeratore = vm.partenzaList.concat(vm.tappeList, vm.arrivoList);
+                for (var i = 0; i < enumeratore.length; i++) {
+                    enumeratore[i].numero = i;
                 }
                 $rootScope.saveOffer = {
-                    partenza:vm.partenzaList,
-                    tappe:vm.tappeList,
-                    arrivo:vm.arrivoList,
-                    dateTimeA:vm.andataTime,
-                    dateTimeR:vm.ritornoTime,
-                    ar:vm.data.group2
-                } 
+                    partenza: vm.partenzaList,
+                    tappe: vm.tappeList,
+                    arrivo: vm.arrivoList,
+                    dateTimeA: vm.andataTime,
+                    dateTimeR: vm.ritornoTime,
+                    ar: vm.data.group2
+                }
                 for (var i = 0; i < enumeratore.length - 1; i++) {
                     promises.push(creaTratta(enumeratore[i], enumeratore[i + 1], i, vm.andataTime.getTime(), 0));
                 }
@@ -94,7 +105,7 @@
                     }
                 }
                 $q.all(promises).then(
-                    function() { 
+                    function () {
                         var viaggio = {
                             utente_fk: vm.utente.id,
                             tipologia: 0
@@ -104,14 +115,17 @@
                             ritorno: vm.ritornoTratta,
                             viaggio: viaggio
                         };
-                    }, 
-                    function() { 
+                    },
+                    function () {
                         $location.path('/error');
                     }
-                ).finally(function() {
+                ).finally(function () {
                     $location.path('/offer2');
                 });
-            } 
+            }
+            else {
+
+            }
         }
         function creaTratta(obj1, obj2, i, date, a) {
             var deferred = $q.defer();
@@ -130,9 +144,9 @@
             for (var j = 0; j < obj1.address_components.length; j++) {
                 var addressType = obj1.address_components[j].types[0];
                 if (componentForm[addressType]) {
-                    if (addressType === 'route') {route1 = obj1.address_components[j][componentForm[addressType]];}
-                    if (addressType === 'locality') {locality1 = obj1.address_components[j][componentForm[addressType]];}
-                    if (addressType === 'premise') {premise1 = obj1.address_components[j][componentForm[addressType]];}
+                    if (addressType === 'route') { route1 = obj1.address_components[j][componentForm[addressType]]; }
+                    if (addressType === 'locality') { locality1 = obj1.address_components[j][componentForm[addressType]]; }
+                    if (addressType === 'premise') { premise1 = obj1.address_components[j][componentForm[addressType]]; }
                 }
             }
             var nome1 = "";
@@ -186,48 +200,48 @@
                 origins: [formatted_address1],
                 destinations: [formatted_address2],
                 travelMode: 'DRIVING',
-            }, 
-            function(response, status){
-                if (response.success === false) {
-                    deferred.reject("fail");
-                    return deferred.promise;
-                } 
-                else {
-                    if (response.rows[0].elements[0].status==='ZERO_RESULTS') {
+            },
+                function (response, status) {
+                    if (response.success === false) {
                         deferred.reject("fail");
                         return deferred.promise;
                     }
-                    else{
-                        distance=response.rows[0].elements[0].distance.value;
-                        duration=response.rows[0].elements[0].duration.value*60000;
-                        prezzo=Math.pow(2,(Math.log(distance/1000,2))-4.2)-(distance/1000000);
-                        tratta={
-                            orario_partenza:date,
-                            enumerazione:i+1,
-                            prezzo:prezzo,
-                            distanza:distance,
-                            posti:vm.posti,
-                            lat_partenza:lat1,
-                            lng_partenza:lng1,
-                            lat_arrivo:lat2,
-                            lng_arrivo:lng2,
-                            denominazione_partenza:nome1,
-                            denominazione_arrivo:nome2,
-                            durata:duration,
-                        };
-                        if(a===0){
-                            vm.dateA=date+duration;
-                            vm.andataTratta.push(tratta);
+                    else {
+                        if (response.rows[0].elements[0].status === 'ZERO_RESULTS') {
+                            deferred.reject("fail");
+                            return deferred.promise;
                         }
-                        else{
-                            vm.dateR=date+duration;
-                            vm.ritornoTratta.push(tratta);
+                        else {
+                            distance = response.rows[0].elements[0].distance.value;
+                            duration = response.rows[0].elements[0].duration.value * 60000;
+                            prezzo = Math.pow(2, (Math.log(distance / 1000, 2)) - 4.2) - (distance / 1000000);
+                            tratta = {
+                                orario_partenza: date,
+                                enumerazione: i + 1,
+                                prezzo: prezzo,
+                                distanza: distance,
+                                posti: vm.posti,
+                                lat_partenza: lat1,
+                                lng_partenza: lng1,
+                                lat_arrivo: lat2,
+                                lng_arrivo: lng2,
+                                denominazione_partenza: nome1,
+                                denominazione_arrivo: nome2,
+                                durata: duration,
+                            };
+                            if (a === 0) {
+                                vm.dateA = date + duration;
+                                vm.andataTratta.push(tratta);
+                            }
+                            else {
+                                vm.dateR = date + duration;
+                                vm.ritornoTratta.push(tratta);
+                            }
+                            deferred.resolve("ok");
+                            return deferred.promise;
                         }
-                        deferred.resolve("ok");
-                        return deferred.promise;
                     }
-                }                        
-            });
+                });
             return deferred.promise;
         }
         function geolocate() {
@@ -245,16 +259,16 @@
                         });
                         vm.autocompleteOptions = {
                             bounds: circle.getBounds(),
-                            types: ['establishment','geocode'],
-                            componentRestrictions: {country: 'it'},
+                            types: ['establishment', 'geocode'],
+                            componentRestrictions: { country: 'it' },
                         }
                     });
                 }
             }
         }
-        Math.log = (function() {
+        Math.log = (function () {
             var log = Math.log;
-            return function(n, base) {
+            return function (n, base) {
                 return log(n) / (base ? log(base) : 1);
             };
         })();
